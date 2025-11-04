097f5c63f8cc3105ae46d0176e1c328bad71a0a6
YukiSato <yukiyukiamam@gmail.com>
2025-11-04 22:56:35 +0900

    Updates

--- _next/static/UP4vWSG5fEf7bLowzXrUn/_buildManifest.js
+++ _next/static/UP4vWSG5fEf7bLowzXrUn/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/libraryOfCoffee/_next/:path+",
+        "destination": "/frontend/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
