1dccb6c27258960df51263768bef4aa3581e8504
YukiSato <yukiyukiamam@gmail.com>
2025-11-15 14:41:09 +0900

    tiny fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,16 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  /* FIXME: 一時的にgithub pagesに公開する都合上のもの、のちに削除 */
-  ...(process.env.NODE_ENV === "development"
-    ? {}
-    : {
-      output: "export",
-      basePath: "/frontend",
-      assetPrefix: "/libraryOfCoffee",
-      distDir: "docs",
-    }),
-  /* ここまで */
 };
 
 export default nextConfig;
