cdce75f0e3992bbf02d7870d51835277aa4895d1
YukiSato <yukiyukiamam@gmail.com>
2025-12-07 17:03:20 +0900

    add metadata

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -13,8 +13,13 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Library Of Coffee",
-  description: "珈琲豆の図書館",
+  title: "豆図書",
+  description: "様々な店舗の中から気になる珈琲豆を選んで、30g×3種類が届くサービス。注文後焙煎の新鮮な豆で、自分好みの一杯を見つけませんか？定額1,500円(税込・送料無料)で、店舗のこだわりや豆の特徴を学びながら様々な珈琲体験を。",
+  openGraph: {
+    title: "豆図書",
+    description: "様々な店舗の中から気になる珈琲豆を選んで、30g×3種類が届くサービス。注文後焙煎の新鮮な豆で、自分好みの一杯を見つけませんか？定額1,500円(税込・送料無料)で、店舗のこだわりや豆の特徴を学びながら様々な珈琲体験を。",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
