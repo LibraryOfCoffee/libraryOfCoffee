63914c4606727057f79ab63a60cacc586ff5ee60
YukiSato <yukiyukiamam@gmail.com>
2025-12-13 19:39:05 +0900

    init backend

---
+++ backend/src/main/kotlin/com/mametosho/MametoshoApplication.kt
@@ -0,0 +1,11 @@
+package com.mametosho
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MametoshoApplication
+
+fun main(args: Array<String>) {
+	runApplication<MametoshoApplication>(*args)
+}
